8afbe9b58249fe567c75be28bf793f958c0e89d2
Stephen Harris <steveofar@hotmail.com>
2020-01-04 16:19:34 -0600

    Add run script and maven wrapper.

--- example/run.sh
+++ example/run.sh
@@ -2,31 +2,35 @@
 
 cd $(dirname $0)
 
-# Generate database metadata.
-java -cp target/dml-gen.jar \
+JAR=../target/dml-gen-jar-with-dependencies.jar
+
+echo Generating database metadata...
+java -cp "$JAR" \
   org.sqljson.DatabaseMetadataMain \
   db/dbmd-pg.props \
   db/dbmd-pg.props \
-  db/dbmd-pg.yaml
+  output/dbmd-pg.yaml
+echo "  Done"
 
-# Generate queries.
-java -cp target/dml-gen.jar \
+echo Generating query SQL and matching Java types...
+java -cp "$JAR" \
   org.sqljson.QueryGeneratorMain \
     --types-language:Java \
     --java-nullability:optwrapped \
     --types-file-header:types-file-imports \
-    db/dbmd-pg.yaml \
+    output/dbmd-pg.yaml \
     query-specs.yaml \
     output \
     output
+echo "  Done"
 
-# Generate mod statements.
-java -cp target/dml-gen.jar \
+echo Generating mod statements...
+java -cp "$JAR" \
   org.sqljson.ModStatementGeneratorMain \
   --types-language:Java \
   --package:org.mymods \
-  db/dbmd-pg.yaml \
+  output/dbmd-pg.yaml \
   mod-specs.yaml \
   output/mod-stmts \
   output/mod-stmts
-
+echo "  Done"
