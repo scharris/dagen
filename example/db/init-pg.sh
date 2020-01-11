a7ff48153fa0bcbaf6cbd77fc874074222207867
Steve Harris <steveofar@hotmail.com>
2020-01-11 15:59:47 -0600

    Name the container and add example of running psql within container.

--- example/db/init-pg.sh
+++ example/db/init-pg.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 DDL_DIR=$(cd $(dirname $0) && pwd)/ddl
 
-docker run -d --rm  -p 5432:5432 --shm-size=256MB \
+docker run -d --name pg --rm  -p 5432:5432 --shm-size=256MB \
   -e POSTGRES_USER=drugs -e POSTGRES_PASSWORD=drugs -e POSTGRES_DB=drugs \
   -v $DDL_DIR/create-schema-pg.sql:/docker-entrypoint-initdb.d/01.sql \
   -v $DDL_DIR/create-schema-objects.sql:/docker-entrypoint-initdb.d/02.sql \
   -v $DDL_DIR/create-test-data-pg.sql:/docker-entrypoint-initdb.d/03.sql \
   postgres:12
 
-# Connect via host psql via:
+# Connect via container embedded psql client via:
+#    docker exec -it pg psql -U drugs
+# or via host psql if postgresql client is installed, via:
 #    psql -h 127.0.0.1 -U drugs
